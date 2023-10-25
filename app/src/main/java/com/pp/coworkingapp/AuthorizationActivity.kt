90d39fa15564f8825c5234bb1794d0869c7a090c
YaEtoTui <sazhin2003.egor@gmail.com>
2023-10-26 00:33:09 +0500

    Initial commit

---
+++ app/src/main/java/com/pp/coworkingapp/AuthorizationActivity.kt
@@ -0,0 +1,11 @@
+package com.pp.coworkingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthorizationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authorization)
+    }
+}
